3384b58a080a51114c44dbc326d1cdc0862a0056
Julián Rojas Bustamante <55417738+Julianrojasbustamante@users.noreply.github.com>
2022-06-25 16:11:30 -0500

    Se desarrolla el carrito de compras

--- src/app/client-view/shoping-cart/shoping-cart.component.ts
+++ src/app/client-view/shoping-cart/shoping-cart.component.ts
@@ -3,6 +3,7 @@ import {DataServices} from "../../services/data.services";
 import {Router} from "@angular/router";
 import {ProductoModel} from "../../models/producto.model";
 import Swal from'sweetalert2';
+import {ProductoShopingCartModel} from "../../models/producto-shoping-cart.model";
 
 @Component({
   selector: 'app-shoping-cart',
@@ -10,9 +11,11 @@ import Swal from'sweetalert2';
   styleUrls: ['./shoping-cart.component.css']
 })
 export class ShopingCartComponent implements OnInit {
-
+  productosCarrito: ProductoShopingCartModel[] = [
+    new ProductoShopingCartModel(1, "TECLADO LOGITECH", 85000, "TECLADO GAMING LOGITECH G915 TKL MECANICO NEGRO RGB", 2),
+    new ProductoShopingCartModel(1, "AEROCOOL SI-5200", 1850000, "EQUIPO AEROCOOL SI-5200 AMD RYZEN 5 4650G", 1),
+  ];
   productos: ProductoModel[] | undefined;
-  confirmarPedido = false;
   total = 0;
   constructor(
     private service:DataServices,
@@ -33,8 +36,8 @@ export class ShopingCartComponent implements OnInit {
 
   calcularTotal(){
     this.total = 0;
-    this.productos?.forEach((producto) => {
-      this.total += producto.precio;
+    this.productosCarrito.forEach((producto) => {
+      this.total += producto.precio * producto.cantidad;
     });
   }
 
@@ -64,7 +67,7 @@ export class ShopingCartComponent implements OnInit {
           'Pronto uno de nuestros asesores se pondrá en contacto contigo para indicarte los medios de pago autorizados.',
           'success'
         )
-        this.productos = undefined;
+        this.eliminarTodosLosProductos();
       } else if (
         /* Read more about handling dismissals below */
         result.dismiss === Swal.DismissReason.cancel
@@ -78,8 +81,57 @@ export class ShopingCartComponent implements OnInit {
     })
   }
 
-  eliminarProducto() {
-    this.productos = undefined;
+  eliminarTodosLosProductos(){
+    this.productosCarrito.splice(0, this.productosCarrito.length);
+  }
+
+  eliminarProducto(producto:ProductoShopingCartModel) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'mx-2 btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: '¿Estas seguro de eliminar el producto '+ producto.nombre +' de tu carrito de compras?',
+      text: "",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Confirmar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'El producto fue eliminado exitosamente de tu carrito de compras!',
+          '',
+          'success'
+        )
+        this.quitarProducto(producto);
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'Tu pedido no fue eliminado de tu carrito de compras',
+          'error'
+        )
+      }
+    })
+  }
+
+  quitarProducto(producto: ProductoShopingCartModel){
+    var counter = 0;
+    this.productosCarrito?.forEach(
+      (product) =>{
+        if (product.id == producto.id)
+          this.productosCarrito?.splice(counter, 1);
+        counter ++;
+      }
+    );
   }
 
   verCatalogo() {
